7d9008d50007999c9bc6ab336c1b87ac965e6643
Muthuram <77574899+Muthuram-coder@users.noreply.github.com>
2022-07-18 11:15:46 +0530

    Add files via upload

---
+++ panic2.py
@@ -0,0 +1,11 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
+
+new_phase = "".join(plist[1:3])
+new_phase = new_phase + "".join([plist[5],plist[4],plist[7],plist[6]])
+
+
+print(plist)
+print(new_phase)
